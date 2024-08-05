e907250c73c3bb44f2cf1ec700db9ba736d42011
Alon Levi <levialon@proton.me>
2024-08-05 16:55:28 +0300

    Update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,9 +6,9 @@ const Banner = () => {
     <div className="banner">
       <div className="announcement-bar">
         <p>
-          <span className="animated-highlight">It's time to escape the matrix, together.</span> 
+          <span className="animated-highlight">It's time to escape the matrix, together. </span> 
           Use <span className="highlight">CRYPTO10</span> for 10% OFF using any crypto! CashApp is enabled. 
-          <a href="https://matrix.tf/tos">matrix.tf/tos</a>
+          <a href="https://matrix.tf/tos"> matrix.tf/tos</a>
         </p>
       </div>
       <img src="/assets/images/hero.gif" alt="Banner" className="banner-image animated-image" />
